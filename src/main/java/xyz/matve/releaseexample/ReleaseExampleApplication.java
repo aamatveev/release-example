00d57ee65a426aa8f7aca7c10a0e48e4ad90a9f1
Andrey Matveev <65maa6@cbr.ru>
2020-08-25 16:23:40 +0500

    init

---
+++ src/main/java/xyz/matve/releaseexample/ReleaseExampleApplication.java
@@ -0,0 +1,13 @@
+package xyz.matve.releaseexample;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ReleaseExampleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ReleaseExampleApplication.class, args);
+	}
+
+}
---
+++ src/test/java/xyz/matve/releaseexample/ReleaseExampleApplicationTests.java
@@ -0,0 +1,13 @@
+package xyz.matve.releaseexample;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ReleaseExampleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
