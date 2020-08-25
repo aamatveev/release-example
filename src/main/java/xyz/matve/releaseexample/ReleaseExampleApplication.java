43c5b3708f878aa5bfcadbca995cb54101d5c41f
Andrey Matveev <65maa6@cbr.ru>
2020-08-25 17:05:16 +0500

    add rest

--- src/main/java/xyz/matve/releaseexample/ReleaseExampleApplication.java
+++ src/main/java/xyz/matve/releaseexample/ReleaseExampleApplication.java
@@ -2,12 +2,22 @@ package xyz.matve.releaseexample;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
+@RestController
 public class ReleaseExampleApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(ReleaseExampleApplication.class, args);
+	}
+
+	@RequestMapping(value = "/test", method = RequestMethod.GET)
+	public ResponseEntity test() {
+		return ResponseEntity.ok("Hello");
 	}
 
 }
